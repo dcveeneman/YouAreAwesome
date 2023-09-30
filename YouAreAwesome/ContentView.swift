d468d872072e160d521f8e3ca602e94f799c119f
David Veeneman <davidv@veeneman.com>
2023-09-30 10:59:09 -0700

    2023:09-30: Gallaugher while-loop code at end of Chapter 1.15

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,15 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageString = ""
     @State private var imageName = ""
-    @State private var lastMessageNumber = 0
-    @State private var currentMessageNumber = 0
-    @State private var lastImageNumber = 0
-    @State private var currentImageNumber = 0
-
-     var body: some View {
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+    
+    var body: some View {
         
         VStack {
-            // Declare Text view
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -29,40 +26,43 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .padding()
             
-            //Declare Image view
+            
             Image(imageName)
                 .resizable()
                 .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+                .padding()
             
             Spacer()
             
-            //Declare Button view
             Button("Show Message") {
-                
+                var newMessageNumber = -1
                 let messages = ["You Are Awesome!",
                                 "You Are Great!",
-                                "Fabulous? That's You!",
-                                "You Are Fantastic!"]
-
-                // Select new message, different from the last
-                while currentMessageNumber == lastMessageNumber {
-                    currentMessageNumber = Int.random(in: 0...3)
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!"]
+                
+                // Generate a random message number to use as an index
+                // If messageNumber == lastMessageNumber {
+                //     Keep generating a new random messageNumber
+                //     until new messageNumber ! lastMessageNumber}
+                // Set messageString to messages[messageNumber]
+                // Update lastMessageNumber with new messageNumber
+                
+                var messageNumber = Int.random(in: 0...messages.count - 1)
+                while messageNumber == lastMessageNumber {
+                    messageNumber = Int.random(in: 0...messages.count - 1)
                 }
-                lastMessageNumber = currentMessageNumber
+                messageString = messages[messageNumber]
+                lastMessageNumber = messageNumber
                 
-                
-                messageString = (messages[currentMessageNumber]) // Triggers UI update
-                
-                // Select new image, different from the last
-                while currentImageNumber == lastImageNumber {
-                    currentImageNumber = Int.random(in: 1...9)
-                    print("Last image number: \(lastImageNumber); Current image number: \(currentImageNumber)")
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber {
+                    imageNumber = Int.random(in: 0...9)
                 }
-                lastImageNumber = currentImageNumber
-
-                print("***")
-
-                imageName = "image\(currentImageNumber)" // Triggers UI update
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
             }
             .buttonStyle(.borderedProminent)
         }
